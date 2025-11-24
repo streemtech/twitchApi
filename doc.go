2726735ba1c57eebabb31410315e4fdf8efa7232
deefdragon <deef@deef.tech>
2025-11-24 17:12:29 -0600

    Add workflow job to auto-update

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 package twitchApi
 
-//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml https://raw.githubusercontent.com/DmitryScaletta/twitch-api-swagger/refs/heads/main/openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml openapi.yaml
 //generate API code.
 
 //go:generate go run github.com/vektra/mockery/v2@v2.52.2 --name ClientWithResponsesInterface --output ./mocks --with-expecter --filename api.gen.go --structname Api
--- makefile
+++ makefile
@@ -3,6 +3,13 @@
 clean:
 	find . -name '*.gen.go' -delete
 
-gen: clean generate
+fetch: 
+	wget -N https://raw.githubusercontent.com/DmitryScaletta/twitch-api-swagger/refs/heads/main/openapi.yaml
+
+gen: clean fetch generate
 generate:
 	GODEBUG=gotypesalias=0 go generate ./...
+
+# Testbuild allows making sure the build is still valid to at-least some extent.
+testbuild:
+	cd cmd && go build -o main
